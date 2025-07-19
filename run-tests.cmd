675618cde84166aefa25905f25ccfd6143dd6b59
Matthew Asplund <mwasplund@gmail.com>
2025-07-19 09:17:53 -0500

    Update tests

--- run-tests.cmd
+++ run-tests.cmd
@@ -2,5 +2,5 @@
 SETLOCAL
 SET RootDir=%~dp0
 
-soup run ..\soup\code\generate-test\ -args %RootDir%\code\run-tests.wren %RootDir%\out\Wren\Local\Cpp\0.16.0\J_HqSstV55vlb-x6RWC_hLRFRDU\script\bundles.sml
+soup run ..\soup\code\generate-test\ -args %RootDir%\code\run-tests.wren %RootDir%\out\Wren\Local\Cpp\0.16.1\J_HqSstV55vlb-x6RWC_hLRFRDU\script\bundles.sml
 if %ERRORLEVEL% NEQ  0 exit /B %ERRORLEVEL%
