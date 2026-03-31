d4fbe020bf39d305c5c0201e416f8a4e805b534d
Matthew Asplund <mwasplund@gmail.com>
2026-03-30 21:48:39 -0700

    Resolve MSVC with versions

--- run-tests.cmd
+++ run-tests.cmd
@@ -2,5 +2,7 @@
 SETLOCAL
 SET RootDir=%~dp0
 
-soup run ..\soup\code\generate-test\ -args %RootDir%\code\run-tests.wren %RootDir%\out\Wren\Local\Cpp\0.16.1\J_HqSstV55vlb-x6RWC_hLRFRDU\script\bundles.sml
+CALL soup build code\extension\
+
+CALL soup run ..\soup\code\generate-test\ -args %RootDir%\code\run-tests.wren %RootDir%\out\Wren\Local\Cpp\0.16.2\J_HqSstV55vlb-x6RWC_hLRFRDU\script\bundles.sml
 if %ERRORLEVEL% NEQ  0 exit /B %ERRORLEVEL%
